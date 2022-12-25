cbf14514e0e4daca64a822434be7f537eb7927fa
Mrunmay-0615 <105450337+Mrunmay-0615@users.noreply.github.com>
2022-12-25 07:50:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,24 @@ const item3 = new Item({
 const defaultItems = [item1, item2, item3];
 
 app.get("/", function(req, res){
-  res.render("list", {listTitle: "Today", newListItems:[]});
+  Item.find({}, function(err, items){
+    if(err){
+      console.log(err);
+    }
+    else{
+      if (items.length===0){
+        Item.insertMany(defaultItems, function(err){
+          if(err){
+            console.log(err);
+          }
+          else{
+            console.log("Successfully added default items.");
+          }
+        });
+      }
+      res.render("list", {listTitle: "Today", newListItems: items});
+    }
+  });
 });
 
 app.listen(port, function() {
